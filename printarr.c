fc2e5fcbe108e76f182ce5405098c5785ea3412f
Christopher Ray <vde435@gmail.com>
2021-09-27 13:15:14 -0500

    Update
    A bit more dynamic, and auto-filling with rand().

--- printarr.c
+++ printarr.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <stdlib.h>
+
 #define SIZE 4
 
 void printarray(int*);
@@ -6,6 +8,18 @@ void printarray(int*);
 int main(void){
 	int x[SIZE] = {243,157,234,6943};
 	printarray(x);
+
+	int size = 0;
+	printf("Enter size for this array: ");
+	scanf("%d", &size);
+	int y[size];
+	printf("%d", size);
+
+	for(int i = 0; i < size; i++){
+		y[i] = rand();
+	}
+
+	printarray(y);
 }
 
 void printarray(int* x){
